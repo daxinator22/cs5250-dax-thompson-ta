6b0cf53062347e56a66f3368def5acbaff9dffa7
Dax Thompson <dax.thompson@aggiemail.usu.edu>
2021-02-22 13:29:14 -0700

    Finished Security Group setup

--- 5hw_grade.py
+++ 5hw_grade.py
@@ -10,9 +10,18 @@ def check_dynamodb_setup(resource):
         print(keys)
 
 def check_sg_setup(sg):
-    print(sg)
+    inbound_rules = sg.ip_permissions[0]
+    outbound_rules = sg.ip_permissions_egress[0]
+    setup = f'''    Security Group Name      :  {sg.group_name}
+        Inbound Rules    :  Port       :  {inbound_rules['FromPort']}
+                            Protocol   :  {inbound_rules['IpProtocol']}
+                            IP Range   :  {inbound_rules['IpRanges'][0]['CidrIp']}
+        Outbound Rules   :  Protocol   :  {outbound_rules['IpProtocol']}
+                            IP Range   :  {outbound_rules['IpRanges'][0]['CidrIp']}'''
+    print(setup)
 
 def check_rds_setup(client):
+    print('Checking RDS Setup...')
     instances = client.describe_db_instances()['DBInstances']
     cs5250_instance = None
     for instance in instances:
@@ -20,9 +29,10 @@ def check_rds_setup(client):
             cs5250_instance = instance
 
     sg = boto3.resource('ec2').SecurityGroup(instance['VpcSecurityGroups'][0]['VpcSecurityGroupId'])
-    setup = f'''Identifier      :  {instance['DBInstanceIdentifier']}'''
+    setup = f'''    Identifier      :  {instance['DBInstanceIdentifier']}'''
     print(setup)
-    check_sg_setup(sg)
+    print(f'{instance}')
+    #check_sg_setup(sg)
 
 dynamodb_resource = boto3.resource('dynamodb')
 rds_client = boto3.client('rds')
